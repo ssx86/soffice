5bafcc6457a339f48a681f3687b7ffeacf0dc05f
SS-X <zss2366@gmail.com>
2012-10-23 00:00:59 +0800

    实现插入文本、列表

---
+++ xmltest/test.py
@@ -0,0 +1,64 @@
+from ooopy.Transformer import Transformer
+from ooopy.OOoPy import OOoPy 
+
+
+from PyQt4 import QtGui
+from PyQt4.QtGui import QTextFormat
+from PyQt4.QtGui import QTextListFormat
+from PyQt4.QtGui import QTextBlockFormat
+from PyQt4.QtGui import QColor
+
+# Create a document object
+doc = QtGui.QTextDocument()
+# Create a cursor pointing to the beginning of the document
+cursor = QtGui.QTextCursor(doc)
+
+# Insert some text
+listFormat = QTextListFormat()
+listFormat.setStyle(QTextListFormat.ListDecimal)
+
+cursor.insertList(listFormat)
+
+cursor.insertText("one\n")
+cursor.insertText("two\n")
+cursor.insertText("three\n")
+
+#cursor.insertBlock()
+#
+#listFormat.clearProperty(QTextFormat.ListStyle)
+#
+#cursor.insertBlock()
+#
+#cursor.insertText("one")
+#cursor.insertText("\ntwo")
+#cursor.insertText("\nthree")
+
+
+
+format = QTextBlockFormat()
+format.setBackground(QColor(255, 0, 0));
+cursor.setBlockFormat(format);
+
+cursor.insertText("the ");
+
+format.setBackground(QColor(255, 255, 0));
+cursor.insertBlock(format);
+cursor.insertText("fish ");
+
+format.setBackground(QColor(0, 0, 255));
+cursor.insertBlock(format);
+cursor.insertText("are ");
+
+format.setBackground(QColor(0, 255, 255));
+cursor.insertBlock(format);
+cursor.insertText("coming!");
+
+
+# Create a writer to save the document
+writer = QtGui.QTextDocumentWriter()
+writer.supportedDocumentFormats()
+#[PyQt4.QtCore.QByteArray(b'HTML'), PyQt4.QtCore.QByteArray(b'ODF'), PyQt4.QtCore.QByteArray(b'plaintext')]
+odf_format = writer.supportedDocumentFormats()[1]
+writer.setFormat(odf_format)
+writer.setFileName('hello_world.odt')
+writer.write(doc) # Return True if successful
